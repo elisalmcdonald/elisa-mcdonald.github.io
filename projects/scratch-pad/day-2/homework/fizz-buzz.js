4d50d787d37716853318ddc2dc5b8c9374d65543
Elisa McDonald <elisalmcdonald@gmail.com>
2023-03-01 04:12:29 +0000

    fizz-buzz completed

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -12,11 +12,26 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
-for (var i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-        console.log(i);
-    } else if ()
-}
+    // loop through numbers 1-100 incrementing by 1
+    for (var i = 1; i <= 100; i++) {// i = 1, 2, 3...100
+        // if input is a multiple of 3 and 5 
+        // print FizzBuzz
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz')
+        // else if input is a multiple of just 3 
+        // print Fizz
+        } else if (i % 3 === 0) {
+            console.log('Fizz');
+        // else if input is a multiple of just 5 
+        // print Buzz
+        } else if (i % 5 === 0) {
+            console.log('Buzz')
+        // else if input isn't a multiple of 3 or 5
+        // print current i
+        } else {
+            console.log(i)
+        }
+    }
 
     // YOUR CODE GOES ABOVE HERE //
 }
