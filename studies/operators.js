1246902d416e54111defacb4eaae7ae55a5ae906
Elisa McDonald <elisalmcdonald@gmail.com>
2023-03-08 18:50:54 +0000

    working on studies

---
+++ studies/operators.js
@@ -0,0 +1,49 @@
+/**
+ * 
+ * OPERATORS:
+ *
+ * 0: Operators are special symbols that act on data. They're used to perform operations on values/variables like 
+ * assign them to a variable, compare them, or used in arithmetic operations.
+ * 
+ * 1. Assignment Operators: Used to assign a value to a variable.
+ *
+ * 2. Arithmetic Operators: Used to perform arthmetic on numbers (litreals or variables).
+ * 
+ * 3. Comparison Operators: Used to compare two values to determine equality or difference and evaluate to true or false. 
+ * Loosely equal to checks if just the value is equal and strictly equal to checks both if the value and data type are equal.
+ * Unless there's a good reason not to use strictly equal to and strictly not equal to.
+ * 
+ * 4. Logical Operators: Used to compare two variables or values and determine the logic whether it's true or false.
+ * 
+ * 5. Unary Operators (!, typeOf, -):
+ * 
+ * 6. Ternary Operator (a ? b : c): 
+ */
+
+// 1.  =, +=, -=, *=, /=, %=, **=
+x = 2 
+x += 2 // => x = x + 2
+x -= 2 // => x = x - 2
+x *= 2 // => x = x * 2
+x /= 2 // => x = x / 2
+x %= 2 // => x = x % 2
+x **= 2 // => x = x ** 2
+
+// 2. 
+
+// 3.  == (loosely equal to), === (strictly equal to), != (loosely not equal to), !== (strictly not equal to), 
+//     > (greater than), < (less than), >= (greater than or equal to, <= (less than or equal to)
+
+var x = 5
+
+
+x > 10 // => false
+x < 10 // => true
+x >= 10 // => false
+x <= 10 // => true 
+
+// 4.  
+
+// 5.
+
+// 6.
--- studies/variables.js
+++ studies/variables.js
@@ -2,20 +2,48 @@
  * 
  * VARIABLES:
  * 
- * 0: Variables are named containers for data in JavaScript. You can declare a variable using 3 
- * different declartive keywords (var, let, const);
+ * 0: Variables are named storage holders for data in JavaScript. You can declare a variable using either var, let, const;
  * 
- * 1. Declaration and assignment: To create a variable in JavaScript, it is best practice to first
- * "declare" the variable using one of the 3 declarative keywords. Once declared, you can then "assign"
- * the variable a value.
+ * 1. Declaration and assignment: To create a variable in JavaScript, "declare" the variable using one of the 3 
+ * declarative keywords. After declaring the variable, you can then "assign" the variable a value. 
+ *
+ * 2. Var, Let, Const: Var, Let, and Const are the 3 declarative keywords. Var can be updated and re-declared,
+ * Let can be updated but not re-declared, and const can't be updated or re-declared.
  * 
- * 2. Var, Let, Const:
- * 
- * 
+ * 3. Hoisting: 
  */
 
-// 1 // Declaration and Assignment 
-var age; // variable age declared
-age = 35; // variable "assigned" value of 35
+// 1. Declaration and Assignment:
+var city; // declaring variable city 
+console.log(city); // prints => undefined  
 
-// 2 // Var, Let, and Const
+city = 'New Orleans'; // assgined variable to the string 'New Orleans' 
+console.log(city); // prints => 'New Orleans'
+
+
+// 2. Var, Let, and Const
+var age = 10; // declaring variable age to the value 10
+console.log(age) // prints => 10
+
+var age = 11; // re-declaring variable age to the value 11
+console.log(age) // prints => 11
+
+let number = 2; // declaring variable number to be assigned to the value 2
+console.log(number) // prints => 2
+
+// let number = 3; won't work because you can't re-declare let 
+console.log(number) // prints => error message 
+
+number = 3; // re-assigning variable number to the value 3
+console.log(number) // prints => 3
+
+const animal = 'cat'; // declaring variable animal to be assigned to the string 'cat'
+console.log(animal) // prints => 'cat'
+
+// const animal = 'dog' // won't work because you can't re-declare const
+console.log(animal) // prints => error message 
+
+// animal = 'bunny' // won't work because you can't update const
+console.log(animal) // prints => error message 
+
+// 3. Hoisting 
